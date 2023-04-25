b21925ede368730e3994dd78d956c41d271ac175
Ismael <lindoespinoso@gmail.com>
2023-04-25 15:56:11 +0200

    v.0.0.2

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -2,27 +2,34 @@ import "@matterlabs/hardhat-zksync-deploy";
 import "@matterlabs/hardhat-zksync-solc";
 require('dotenv').config();
 
-const key = process.env.ALCHEMY_KEY;
+const key = process.env.ALCHEMY_URL;
+const priv = process.env.PRIVATE_KEY
 
 module.exports = {
-  zksolc: {
-    version: "1.3.5",
-    compilerSource: "binary",
-    settings: {},
-  },
+zksolc: { 
+  version: "1.3.8", 
+  compilerSource: "binary", 
+  settings: { 
+    optimizer: { 
+      enabled: true, 
+      mode: "z", 
+    }, 
+  }, 
+}, 
   defaultNetwork: "zkSyncTestnet",
 
   networks: {
-    zkSyncTestnet: {
-      url: "https://zksync2-testnet.zksync.dev",
-      ethNetwork: "goerli", // Can also be the RPC URL of the network (e.g. `https://goerli.infura.io/v3/<API_KEY>`)
-      zksync: true,
+    goerli:{
+      url: `${key}`,
+      accounts: [`${priv}`],
     },
-    goerli: {
-      url:`https://eth-goerli.g.alchemy.com/v2/${key}`
+    zkSyncTestnet: {
+      url: "https://testnet.era.zksync.dev",
+      ethNetwork: "goerli", // RPC URL of the network (e.g. `https://goerli.infura.io/v3/<API_KEY>`)
+      zksync: true,
     },
   },
   solidity: {
-    version: "0.8.17",
+    version: "0.8.8",
   },
 };
